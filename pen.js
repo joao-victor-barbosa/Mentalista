109b3bdeef8a0cb0d522d541b0bcd2ff54da1d21
joao-victor-barbosa <joao.v.ferreira.barbosa@gmail.com>
2022-09-08 20:48:48 -0300

    Update pen.js

--- pen.js
+++ pen.js
@@ -1,11 +1,16 @@
-var numeroSecreto = 3;
+var numeroSecreto = parseInt(Math.random() * 11);
 
 function Chutar() {
+ var elementoResultado = document.getElementById("resultado");
  var chute = parseInt(document.getElementById("valor").value);
   console.log(chute);
   if (chute == numeroSecreto){
-    console.log("Acertou"); 
-  } else {
-    console.log("Errou");
+    elementoResultado.innerHTML = "Parabens você acertou :)"; 
+  } 
+  else if (chute > 10 || chute < 0){
+    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10!"
+  } 
+  else {
+   elementoResultado.innerHTML = "Errou";
   }
 } 
